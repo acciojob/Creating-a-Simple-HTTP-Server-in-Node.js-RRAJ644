ad7f1450550365a26d58c8523535d0ed67f13f76
RRAJ644 <riturajsingh.17248@gmail.com>
2024-11-17 14:27:25 +0530

    request

--- main.js
+++ main.js
@@ -2,14 +2,11 @@ const http = require('http')
 const port = 3000
 
 const server = http.createServer((req, res) => {
-  // TODO: Send the ""Hello, world!"" response
-
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello world!')
+  res.write('Hello, world!')
+  res.end()
 })
 
 server.listen(port, () => {
-  // TODO: Log a message to the console when the server starts listening on the port
   console.log(`Server is listening on port ${port}`)
 })
 
